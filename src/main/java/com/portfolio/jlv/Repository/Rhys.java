f0bf3bb7d96b46558464533344491d92ad98e3e7
jonavia <jonatanviale1@outlook.com.ar>
2023-01-06 15:38:54 -0300

    back

---
+++ src/main/java/com/portfolio/jlv/Repository/Rhys.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.jlv.Repository;
+
+import com.portfolio.jlv.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
---
+++ src/main/java/com/portfolio/jlv/jlvApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jlv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class jlvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(jlvApplication.class, args);
+	}
+
+}
